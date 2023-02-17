87cdd3b7293e9b35a5ffda44e0077d3b17127b13
Lorenzo Castellino <castellino96@gmail.com>
2023-02-17 15:27:34 +0100

    Fix numpy import to follow contributing guidelines

--- pymcr/metrics.py
+++ pymcr/metrics.py
@@ -4,7 +4,7 @@ Metrics used in pyMCR
 All functions must take C, ST, D_actual, D_calculated as NDArrays
 and return a single NDArray as a result
 """
-import numpy as np
+import numpy as _np
 from numpy.typing import NDArray
 
 
@@ -14,6 +14,6 @@ def mse(C: NDArray, ST: NDArray, D_actual: NDArray, D_calculated: NDArray) -> ND
 
 
 def lof(C: NDArray, ST: NDArray, D_actual: NDArray, D_calculated: NDArray) -> NDArray:
-    return 100 * np.sqrt(
-        np.sum((D_actual - D_calculated) ** 2) / np.sum((D_actual**2))
+    return 100 * _np.sqrt(
+        _np.sum((D_actual - D_calculated) ** 2) / _np.sum((D_actual**2))
     )
